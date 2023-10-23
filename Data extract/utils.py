e8a100006e94e0dc99ef2c4a0e2eb1d66f77122b
xiao-zy19 <2563590191@qq.com>
2023-10-24 03:39:01 +0800

    Add files via upload
    added func temp and func normal_temperature, return with patient temperature in Degree Celsius

--- Data extract/utils.py
+++ Data extract/utils.py
@@ -8,7 +8,7 @@ import pandas as pd
 def patient_id_age():
     """
     Function to extract patient id and age.
-    
+
     Parameters: None
     Return:
         patient_id: the list of wanted patient id
@@ -86,17 +86,15 @@ def heart_rate(
 
     Parameters:
         patient_id: the list of wanted patient id
-        file_name1: the file name of vitalPeriodic.csv
-        file_name2: the file name of nurseCharting.csv
+        file_name1: the file path of vitalPeriodic.csv
+        file_name2: the file path of nurseCharting.csv
     Return:
         HR: the dataframe of heart rate data, including patientunitstayid, observationoffset, heartrate
         HR_index: the series of the index of the first occurrence of each patient
+    Usage:
+        If we want the i th patient data (i starts from 0)
+        use HR.iloc[HR_index[i]:HR_index[i+1]]
     """
-    # usage:
-    # If we want the i th patient data (i starts from 0)
-    # use HR.iloc[HR_index[i]:HR_index[i+1]]
-    # i = 0 # the first patient
-    # print(f'HeartRate data for patient {i+1}: \n{HR.iloc[HR_index[i]:HR_index[i+1]]}')
 
     print("Loading Heart Rate Data...")
     start_time = time.time()
@@ -158,6 +156,87 @@ def heart_rate(
     return HR, HR_index
 
 
+def temp(
+    patient_id, file_name1="data/vitalPeriodic.csv", file_name2="data/nurseCharting.csv"
+):
+    """
+    Function to extract temperature values.
+
+    Parameters:
+        patient_id: the list of wanted patient id
+        file_name1: the file path of vitalPeriodic.csv
+        file_name2: the file path of nurseCharting.csv
+    Return:
+        Temp: the dataframe of temperature data, including patientunitstayid, observationoffset, temperature
+        Temp_index: the series of the index of the first occurrence of each patient
+    Usage:
+        If we want the i th patient data (i starts from 0)
+        use Temp.iloc[Temp_index[i]:Temp_index[i+1]]
+    """
+    print("Loading Temperature Data...")
+    start_time = time.time()
+
+    # import vitalPeriodic.csv & nurseCharting.csv
+    df_vitalPeriodic = pd.read_csv(file_name1)
+    df_vitalPeriodic.sort_values(
+        by=["patientunitstayid", "observationoffset"], inplace=True
+    )
+    df_nurseCharting = pd.read_csv(file_name2)
+    df_nurseCharting.sort_values(
+        by=["patientunitstayid", "nursingchartoffset"], inplace=True
+    )
+
+    # select the wanted patient
+    df_vitalPeriodic = df_vitalPeriodic[
+        df_vitalPeriodic["patientunitstayid"].isin(patient_id)
+    ]
+    df_nurseCharting = df_nurseCharting[
+        df_nurseCharting["patientunitstayid"].isin(patient_id)
+    ]
+
+    # nursingchartcelltypevallabel Temperature
+    df_nurseCharting = df_nurseCharting[
+        df_nurseCharting["nursingchartcelltypevalname"] == "Temperature (C)"
+    ]
+    df_nurseCharting = df_nurseCharting.rename(
+        columns={
+            "nursingchartoffset": "observationoffset",
+            "nursingchartvalue": "temperature",
+        }
+    )
+
+    # extract temperature from df_vitalPeriodic & df_nurseCharting
+    Temp_v = df_vitalPeriodic[["patientunitstayid", "observationoffset", "temperature"]]
+    Temp_n = df_nurseCharting[["patientunitstayid", "observationoffset", "temperature"]]
+
+    # delete the rows with string values
+    Temp_n = Temp_n[
+        Temp_n["temperature"].apply(lambda x: str(x).replace(".", "", 1).isdigit())
+    ]
+
+    Temp = pd.concat([Temp_v, Temp_n]).astype(float)
+    Temp.sort_values(by=["patientunitstayid", "observationoffset"], inplace=True)
+
+    # exclude abnormal temp values and convert Fahrenheit to Celsius
+    Temp.loc[:, "temperature"] = Temp["temperature"].apply(normal_temperature)
+
+    value_position_dict = {}
+    first_occurrences = []
+    for idx, value in enumerate(Temp["patientunitstayid"]):
+        # if the value is not in the dictionary, add it and create index
+        if value not in value_position_dict:
+            value_position_dict[value] = idx
+            first_occurrences.append(idx)
+
+    first_occurrences.append(len(Temp))
+    # create first occurrence index for every patient
+    Temp_index = pd.Series(first_occurrences)
+    end_time = time.time()
+    print(f"Temperature Data Loaded. Time: {end_time - start_time:.2f}s")
+
+    return Temp, Temp_index
+
+
 def normal_heartrate(num):
     """
     Function to normalize heart rate values.
@@ -172,6 +251,31 @@ def normal_heartrate(num):
         return num
     # Remove values out of range
     elif num > 300 or num < 0:
+        return np.nan
+    # Return normal values directly
+    else:
+        return num
+
+
+# define normal temp function
+def normal_temperature(num):
+    """
+    Function to normalize temperature values.
+
+    Parameters:
+        num: the originial input value
+    Return:
+        num: the normalized output value
+    """
+    # Return null values direcly
+    if pd.isna(num):
+        return num
+    # Convert Fahrenheit to Celsius
+    # And apply the function again
+    elif num > 50:
+        return normal_temperature((num - 32) * 5 / 9)
+    # Remove values out of range
+    elif num < 15 or num > 45:
         return np.nan
     # Return normal values directly
     else:
