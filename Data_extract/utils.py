5a5369cbc0377d8d8bb41984c4e9fbd413e32992
xiao-zy19 <2563590191@qq.com>
2023-11-29 19:14:50 -0500

    fixed plt float bug

--- Data_extract/utils.py
+++ Data_extract/utils.py
@@ -868,8 +868,8 @@ def align_data(
                 column_names[0]
             ].unique()[0]
 
-            t = data_data[:, 0]
-            y = data_data[:, 1]
+            t = data_data[:, 0].astype('float64')
+            y = data_data[:, 1].astype('float64')
             t_known = t[~np.isnan(y)]
             y_known = y[~np.isnan(y)]
             t_missing = t[np.isnan(y)]
