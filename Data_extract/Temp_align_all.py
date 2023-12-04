455d34e24c0d6caca66254679e3996ca50d1c42e
xiao-zy19 <2563590191@qq.com>
2023-12-04 11:36:56 -0500

    file upload

--- Data_extract/Temp_align_all.py
+++ Data_extract/Temp_align_all.py
@@ -4,15 +4,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 import time
-from utils import patient_id_age, heart_rate, align_data
+from utils import patient_id_age, heart_rate, align_data, temp
 
 
 start_time_all = time.time()
 
 # os.chdir('/Users/xiao-zy19/Desktop/Johns Hopkins/Biomedical Data Design/EICU Database/eicu-collaborative-research-database-demo-2.0.1') 
-# os.chdir('/home/en580-zxia028/EICU_demo/data')
 os.chdir('/home/en580-zxia028/EICU/data')
-folder_path = '/home/en580-zxia028/output/hr'
+folder_path = '/home/en580-zxia028/output/temp'
 
 # get patient id
 patient_id, patient_age, patient_offset = patient_id_age()
@@ -36,23 +35,21 @@ if batch_size.lower() == 'all':
     batch_size = len(patient_id)  # set batch_size to be the number of patients
     patient_batch = [patient_id[i:i + batch_size] for i in range(0, len(patient_id), batch_size)]
     # patient_batch = patient_id
-    file_name = 'HR_all.csv'
+    file_name = 'temp_all.csv'
     print('batch size: ', batch_size)
     print('now processing all data')
 else:
     batch_size = int(batch_size)  # turn batch_size into integer
     patient_batch = [patient_id[i:i + batch_size] for i in range(0, len(patient_id), batch_size)]
-    file_name = 'HR_full_' + str(batch) + '.csv'
+    file_name = 'temp_full_' + str(batch) + '.csv'
     print('batch size: ', batch_size)
     print('now processing batch: ', batch, '/', len(patient_batch)-1)
 
 print(patient_batch[batch].shape)
 # extract heart rate data
-HR, _ = heart_rate(patient_batch[batch], drop_neg=True)
-data_full, data_full_index = align_data(patient_batch[batch], patient_offset, HR, graph=False)
+Temp, _ = temp(patient_batch[batch])
+data_full, data_full_index = align_data(patient_batch[batch], patient_offset, Temp, graph=False)
 
-# folder_path = '/home/en580-zxia028/output/hr/data_full'
-# file_name = 'HR_full_' + str(batch) + '.csv'
 data_full.to_csv(os.path.join(folder_path, file_name), index=False)
 
 end_time_all = time.time()
